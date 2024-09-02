051dd302ae4d4804ef4e2651b0104914bfff3700
DivyaDaram8 <divyadaram0416@gmail.com>
2024-09-02 07:33:00 +0530

    Time: 2 ms (52.18%), Space: 41 MB (97.57%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
-        ArrayDeque<Character> stk = new ArrayDeque<>();
-        for(int i=0;i<s.length();i++){
-            char x = s.charAt(i);
-            if(x=='(' || x=='{' || x=='[')
-            stk.push(x);
-            else{
-                if(stk.isEmpty()==true)
-                return false;
-                else if(isMatching(stk.peek(),x) == false)
-                return false;
-                else
-                stk.pop();
-            }
-        } 
-           return (stk.isEmpty()==true);
-    }
-    public boolean isMatching(char a,char b){
-        return ((a=='(' && b==')') || (a=='{' && b=='}') || (a=='[' && b==']'));
+        Stack<Character> stk = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+        char c = s.charAt(i);
+        if(c == '(' || c == '[' || c == '{')
+            stk.push(c);
+        else if(c == ')' && !stk.isEmpty() && stk.peek() == '(')
+            stk.pop();
+        else if(c == ']' && !stk.isEmpty() && stk.peek() == '[')
+            stk.pop();
+        else if(c == '}' && !stk.isEmpty() && stk.peek() == '{')
+            stk.pop();
+        else
+            return false;
+        }
+        return stk.isEmpty();
     }
 }
