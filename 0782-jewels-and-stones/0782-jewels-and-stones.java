a0d701a44910ecfa3f1a0413e2a46726b5b07841
DivyaDaram8 <divyadaram0416@gmail.com>
2024-09-23 18:16:11 +0530

    Time: 0 ms (100%), Space: 41.6 MB (80.08%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.java
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -1,24 +1,24 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        // int count = 0;
-        // int[] arr = new int[58];
-        // char[] jewelsArr = jewels.toCharArray();
-        // for(char ch: jewelsArr){
-        //     arr[ch - 'A'] = 1;
-        // }
-        // for(char ch: stones.toCharArray()){
-        //     count+=arr[ch - 'A'];
-        // }
-        // return count;
-        HashSet<Character> jewelSet = new HashSet<>();
         int count = 0;
-        for(char ch: jewels.toCharArray()){
-            jewelSet.add(ch);
+        int[] arr = new int[58];
+        char[] jewelsArr = jewels.toCharArray();
+        for(char ch: jewelsArr){
+            arr[ch - 'A'] = 1;
         }
         for(char ch: stones.toCharArray()){
-            if(jewelSet.contains(ch))
-                count++;
+            count+=arr[ch - 'A'];
         }
         return count;
+        // HashSet<Character> jewelSet = new HashSet<>();
+        // int count = 0;
+        // for(char ch: jewels.toCharArray()){
+        //     jewelSet.add(ch);
+        // }
+        // for(char ch: stones.toCharArray()){
+        //     if(jewelSet.contains(ch))
+        //         count++;
+        // }
+        // return count;
     }
 }
