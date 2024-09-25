a90c286f9e38c12feb30e3527ad9fde17f8c0630
DivyaDaram8 <divyadaram0416@gmail.com>
2024-09-25 22:45:00 +0530

    Time: 0 ms (100%), Space: 41.5 MB (81.51%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,9 +1,9 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         String[] newstr = s.split(" ");
-        // int len = newstr.length -1;
-        // String word = newstr[len];
-        // return word.length();
-        return newstr[newstr.length - 1].length();
+        int len = newstr.length -1;
+        String word = newstr[len];
+        return word.length();
+    //     return newstr[newstr.length - 1].length();
     }
 }
