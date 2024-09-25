0856111e0b08055d60cb295160c412c476f94b7f
DivyaDaram8 <divyadaram0416@gmail.com>
2024-09-25 21:38:49 +0530

    Time: 3 ms (73.02%), Space: 43.1 MB (64.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,14 +1,34 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        // s = s.toLowerCase();
+        // s = s.replaceAll("[^a-z0-9]","");
+        // if(s.length() == 0 || s.length() == 1)
+        //     return true;
+        // for(int i = 0; i < s.length(); i++){
+        //     char start = s.charAt(i);
+        //     char end = s.charAt(s.length() - i - 1);
+        //     if(start != end){
+        //         return false;
+        //     }
+        // }
+        // return true;
         s = s.toLowerCase();
-        s = s.replaceAll("[^a-z0-9]","");
-        if(s.length() == 0 || s.length() == 1)
+        if(s.isEmpty())
             return true;
-        for(int i = 0; i < s.length(); i++){
-            char start = s.charAt(i);
-            char end = s.charAt(s.length() - i - 1);
-            if(start != end){
-                return false;
+        int start = 0;
+        int end = s.length() - 1;
+        while(start <= end){
+            char EleAtStart = s.charAt(start);
+            char EleAtEnd = s.charAt(end);
+            if(!Character.isLetterOrDigit(EleAtStart))
+                start++;
+            else if(!Character.isLetterOrDigit(EleAtEnd))
+                end--;
+            else{
+                if(s.charAt(start) != s.charAt(end))
+                    return false;
+                start++;
+                end--;            
             }
         }
         return true;
