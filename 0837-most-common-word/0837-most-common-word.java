4848739a210ac70dacb87dc69b6bb01389375e0b
DivyaDaram8 <divyadaram0416@gmail.com>
2024-09-30 22:30:29 +0530

    Time: 19 ms (21.55%), Space: 42.9 MB (56.58%) - LeetHub

---
+++ 0837-most-common-word/0837-most-common-word.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        String str = paragraph.toLowerCase().replaceAll("[^a-z]"," ");
+        String[] strArr = str.split("\\s+");
+        Map<String, Integer> mp = new HashMap<>();
+        for(String word : strArr){
+           mp.put(word,mp.getOrDefault(word,0)+1); 
+        }
+        for(String ban : banned){
+            if(mp.containsKey(ban))
+                mp.remove(ban);
+        }
+        String ans = "";
+        int max = 0;
+        for(String word : mp.keySet()){
+            int count = mp.get(word);
+            if(count > max){
+                max = count;
+                ans = word;
+            }
+        }
+        return ans;
+
+    }
+}
