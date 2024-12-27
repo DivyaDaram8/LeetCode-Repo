9724505a314d7954f67d40774f06257c090184c0
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2024-12-27 18:54:47 +0530

    Time: 1 ms (99.89%), Space: 42.1 MB (44.93%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.java
+++ 3379-score-of-a-string/3379-score-of-a-string.java
@@ -2,7 +2,7 @@ class Solution {
     public int scoreOfString(String s) {
         int ans=0;
         for(int i = 0; i < s.length() - 1; i++){
-            ans = ans + Math.abs(s.charAt(i+1) - s.charAt(i));
+            ans = ans + Math.abs(s.charAt(i) - s.charAt(i+1));
         }
         return ans;
     }
