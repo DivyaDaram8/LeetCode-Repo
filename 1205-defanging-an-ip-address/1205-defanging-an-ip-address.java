a564ad36de3f6f6bc00086eb128876cbe2b2615e
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2024-12-30 22:03:21 +0530

    Time: 5 ms (35.69%), Space: 41.9 MB (20.8%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -1,23 +1,14 @@
 class Solution {
     public String defangIPaddr(String address) {
-        // String ans= "";
-        // for( int i = 0; i < address.length(); i++){
-        //     char ch = address.charAt(i);
-        //     if(ch == '.'){
-        //         ans += "[.]";
-        //     }
-        //     else
-        //         ans+= ch;
-        // }
-        // return ans;
-        StringBuilder str = new StringBuilder();
-        for(int i = 0; i < address.length(); i++){
+        String newIp ="";
+        for(int i = 0; i< address.length(); i++){
             if(address.charAt(i) == '.'){
-                str.append("[.]");
+                newIp+= "[.]";
             }
-            else
-                 str.append(address.charAt(i));  
-         }
-         return str.toString();
-    }
+            else{
+                newIp += address.charAt(i);
+            }
+        }
+        return newIp;
+    }  
 }
