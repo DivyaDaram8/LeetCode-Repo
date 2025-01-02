dd4fcd89d5102cf2a36b353b4714d8a734b41ac8
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-01-02 17:56:00 +0530

    Time: 3 ms (58.37%), Space: 47 MB (7.75%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.java
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.java
@@ -4,6 +4,7 @@ class Solution {
         int maxTemp = arr[n-1];
         int temp;
         arr[n-1] = -1;
+        if(n == 1) return arr;
         for(int i = n -2; i >= 0; i--){
             temp = arr[i];
             arr[i] = maxTemp;
