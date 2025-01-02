c356f8ca6aaef670e00a69eff98a96b20eec2bef
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-01-02 18:34:20 +0530

    Time: 4 ms (31.1%), Space: 42.2 MB (90%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.java
@@ -1,9 +1,18 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
+        // int count = Integer.MIN_VALUE;
+        // for(int i = 0; i < sentences.length; i++){
+        //     String[] words = sentences[i].trim().split("\\s+");
+        //     count = Math.max(count,words.length);
+        // }
+        // return count;
         int count = Integer.MIN_VALUE;
-        for(int i = 0; i < sentences.length; i++){
-            String[] words = sentences[i].trim().split("\\s+");
-            count = Math.max(count,words.length);
+        for(int i = 0 ; i < sentences.length; i++){
+            int tempCount = 0;
+            for(int j = 0; j < sentences[i].length(); j++){
+                if(sentences[i].charAt(j) == ' ') tempCount++;
+            }
+            count = Math.max(count, tempCount+1);
         }
         return count;
     }
