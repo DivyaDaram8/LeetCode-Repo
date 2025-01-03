3b4debc089575521cc2551e9f4e7f60d797281ad
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-01-03 18:01:33 +0530

    Time: 15 ms (23.8%), Space: 49.6 MB (77.05%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,41 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
-//         HashMap<Integer, Integer> mp = new HashMap<>();
-//         for( int i = 0; i < nums.length ; i++){
-//             int value = mp.getOrDefault(nums[i],0);
-//            mp.put(nums[i], value + 1);
-//          }
-//         for(Map.Entry<Integer, Integer> it : mp.entrySet()){
-//             if(it.getValue() > (nums.length/2))
-//             return it.getKey();
-//         }
-//         return -1;
-//     }
-// }
-        int count = 0;
-        int ele = 0;
-        for(int i = 0; i< nums.length; i++){
-            if(count == 0){
-                count = 1;
-                ele = nums[i];
-            }
-            else if( nums[i] == ele){
-                count++;
+        HashMap <Integer,Integer> hm = new HashMap<>();
+        for(int i = 0 ; i < nums.length; i++){
+            if(hm.containsKey(nums[i])){
+                hm.put(nums[i], hm.get(nums[i]) + 1);
             }
             else{
-                count--;
+                hm.put(nums[i],1);
             }
         }
-        // int countEle = 0;
-        // for(int i = 0; i < nums.length; i++){
-        //     if(ele == nums[i])
-        //     countEle++;
-        // } 
-        // if(countEle > (nums.length / 2))
-            return ele;  
-
-// //         // Arrays.sort(nums);
-// //         // return nums[nums.length / 2];
-// //         // return -1;
+        int k = 0;
+        for(int key: hm.keySet()){
+            if( hm.get(key) > nums.length /2){
+                k =  key;
+                break;
+            }
+        }
+        return k;
     }
 }
