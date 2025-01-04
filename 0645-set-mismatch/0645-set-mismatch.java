1b77161864b0b9d8278ec979040665dc20a4a86f
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-01-04 22:52:14 +0530

    Time: 2 ms (95.71%), Space: 46 MB (20.67%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,19 +1,24 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        HashSet <Integer> hs = new HashSet<>();
+        int n = nums.length;
+        int[] frequency = new int[n + 1]; // Frequency array for numbers 1 to n
         int repeated = -1;
         int missing = -1;
-        for(int num : nums){
-            if(hs.contains(num)){
-                repeated = num;
-            }
-            hs.add(num);
+
+        // Count the frequency of each number in nums
+        for (int num : nums) {
+            frequency[num]++;
         }
-        for(int i = 1; i < nums.length + 1; i++){
-            if(!hs.contains(i)){
-                missing = i ;
+
+        // Identify repeated and missing numbers
+        for (int i = 1; i <= n; i++) {
+            if (frequency[i] == 2) {
+                repeated = i;
+            } else if (frequency[i] == 0) {
+                missing = i;
             }
         }
-        return new int[]{repeated,missing};
+
+        return new int[]{repeated, missing};
     }
 }
