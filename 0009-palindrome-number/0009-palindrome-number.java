9503431254ebe1f92ffc75e47c8237288169dc15
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-01-05 22:42:56 +0530

    Time: 4 ms (100%), Space: 43.9 MB (79.74%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if ( x < 0){
+            return false;
+        }
+        int revNum = 0;
+        int original = x;
+        while(x > 0){
+            int lastDigit = x % 10;
+            revNum = revNum * 10 + lastDigit;
+            x = x/10;
+        }
+        return revNum == original;
+    }
+}
