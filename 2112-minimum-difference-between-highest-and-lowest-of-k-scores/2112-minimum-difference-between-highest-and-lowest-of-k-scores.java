3dcd68b082836dd431155f8cc0516f33b2ecddaf
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-01-08 18:09:34 +0530

    Time: 5 ms (97.86%), Space: 44.8 MB (20.05%) - LeetHub

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/2112-minimum-difference-between-highest-and-lowest-of-k-scores.java
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/2112-minimum-difference-between-highest-and-lowest-of-k-scores.java
@@ -1,18 +1,32 @@
+// class Solution {
+//     public int minimumDifference(int[] nums, int k) {
+//         Arrays.sort(nums);
+//         if(nums.length == 1) return 0;
+//         int min_diff = Integer.MAX_VALUE;
+//         int start = 0;
+//         for(int end = 0; end < nums.length; end++){
+//             if(end-start == k){
+//                 start++;
+//             }
+//             if(end-start+1 == k){
+//                 int temp_diff = nums[end] - nums[start];
+//                 min_diff = Math.min(min_diff, temp_diff);
+//             }
+//         }
+//         return min_diff;
+//     }
+// }
 class Solution {
     public int minimumDifference(int[] nums, int k) {
         Arrays.sort(nums);
-        if(nums.length == 1) return 0;
-        int min_diff = Integer.MAX_VALUE;
-        int start = 0;
-        for(int end = 0; end < nums.length; end++){
-            if(end-start == k){
-                start++;
-            }
-            if(end-start+1 == k){
-                int temp_diff = nums[end] - nums[start];
-                min_diff = Math.min(min_diff, temp_diff);
-            }
+        int left = 0, right = k - 1;
+        int result = Integer.MAX_VALUE;
+        while(right < nums.length) {
+            result = Math.min(result, nums[right] - nums[left]);
+            left++;
+            right++;
         }
-        return min_diff;
+
+        return result;
     }
 }
