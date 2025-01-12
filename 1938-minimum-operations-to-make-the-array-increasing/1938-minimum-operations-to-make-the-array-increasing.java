7dc7bcdd87d4d7b3f11f3d9bbad51e7b4ee1abe5
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-01-12 23:06:38 +0530

    Time: 4 ms (73.46%), Space: 45 MB (73.58%) - LeetHub

--- 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/1938-minimum-operations-to-make-the-array-increasing.java
@@ -3,7 +3,7 @@ class Solution {
         int cnt = 0;
         for(int i = 0; i < nums.length - 1; i++){
             if(nums[i] >= nums[i+1]){
-                int cntTemp  = Math.abs(nums[i+1] - nums[i]) + 1;
+                int cntTemp  = (nums[i] - nums[i+1]) + 1;
                 nums[i+1] = nums[i+1] + cntTemp;
                 cnt += cntTemp;
             }
