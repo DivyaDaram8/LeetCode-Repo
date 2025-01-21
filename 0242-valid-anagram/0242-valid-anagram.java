8b41c436bef28edbcd78a3493a180c31c44218c5
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-01-21 22:34:43 +0530

    Time: 14 ms (31.2%), Space: 44.9 MB (26.15%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        HashMap <Character, Integer> hms = new HashMap<>();
+        HashMap <Character, Integer> hmt = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            hms.put(ch, hms.getOrDefault(ch,0) + 1);
+        }
+        for(char ch : t.toCharArray()){
+            hmt.put(ch, hmt.getOrDefault(ch,0) + 1);
+        }
+        for(char key : hms.keySet()){
+            if(!hmt.containsKey(key)){
+                return false;
+            }
+            if(!hms.get(key).equals(hmt.get(key))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
