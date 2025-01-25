1c89cd9a4be714d122d7a29ca1e6ff1cd668ca54
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-01-25 22:18:25 +0530

    Time: 1 ms (99.93%), Space: 42.3 MB (52.37%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int x =  a.length() - 1;
+        int y = b.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while( x >= 0 || y >= 0 || carry == 1){
+            if(x >= 0){
+                carry += a.charAt(x--) - '0';
+            }
+            if(y >= 0){
+                carry += b.charAt(y--) - '0';
+            }
+            sb.append(carry % 2);
+            carry /= 2;
+        }
+        return sb.reverse().toString();
+    }
+}
