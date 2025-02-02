064b4fc20453113ec2e88436fa27bd7fb057703c
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-02-02 15:15:00 +0530

    Time: 0 ms (100%), Space: 45.2 MB (37.83%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,39 +1,39 @@
 class Solution {
     public int maximumCount(int[] nums) {
-        // int begin = 0;
-        // int end = nums.length - 1;
-        // while(begin <= end){
-        //     int mid = end + (begin - end)/2;
-        //     if(nums[mid] < 0){
-        //         begin = mid + 1;
-        //     }
-        //     else end = mid - 1;
-        // } 
-        // int neg = end + 1;
-
-        // begin = 0;
-        // end = nums.length - 1;
-        // while(begin <= end){
-        //     int mid = end + (begin - end)/2;
-        //     if(nums[mid] > 0){
-        //         end = mid - 1;
-        //     }
-        //     else{
-        //         begin = mid + 1;
-        //     }
-        // }
-        // int pos = nums.length - begin;
-        // return Math.max(neg,pos);
-        int neg = 0;
-        int pos = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] < 0 ){
-                neg++;
+        int begin = 0;
+        int end = nums.length - 1;
+        while(begin <= end){
+            int mid = end + (begin - end)/2;
+            if(nums[mid] < 0){
+                begin = mid + 1;
             }
-            else if(nums[i] > 0){
-                pos++;
+            else end = mid - 1;
+        } 
+        int neg = end + 1;
+
+        begin = 0;
+        end = nums.length - 1;
+        while(begin <= end){
+            int mid = end + (begin - end)/2;
+            if(nums[mid] <= 0){
+                begin = mid + 1;
+            }
+            else{
+                end = mid - 1;
             }
         }
+        int pos = nums.length - begin;
         return Math.max(neg,pos);
+        // int neg = 0;
+        // int pos = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(nums[i] < 0 ){
+        //         neg++;
+        //     }
+        //     else if(nums[i] > 0){
+        //         pos++;
+        //     }
+        // }
+        // return Math.max(neg,pos); 
     }
 }
