58cae956d64fe56487c949f2b6059213343dc532
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-02-08 23:53:34 +0530

    Time: 1 ms (45.91%), Space: 40.9 MB (70.74%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,5 +1,15 @@
 class Solution {
     public int addDigits(int num) {
-        return 1 + (num - 1) % 9;
+        // return 1 + (num - 1) % 9;
+        // return (num % 9 == 0 && num != 0) ? 9 : num % 9;
+        while(num >= 10){
+            int sum = 0;
+            while(num > 0){
+                sum += num % 10;
+                num = num / 10;
+            }
+            num = sum;
+        }
+        return num;
     }
 }
