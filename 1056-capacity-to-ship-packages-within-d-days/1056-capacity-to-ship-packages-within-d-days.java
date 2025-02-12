9a4af1ed16b68d12204dba749ede8ef1a319c849
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-02-12 19:05:45 +0530

    Time: 10 ms (92.62%), Space: 47.4 MB (47.42%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPossible(int[] weights, int days, int mid){
+        int cnt = 0;
+        for(int i = 0; i < weights.length; i++){
+            int val = weights[i];
+            if(val > mid) return false;
+            if(cnt + val > mid){
+                days -= 1;
+                cnt = 0;
+            }
+            cnt += val;
+            if(days <= 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int begin = 1;
+        int end = Integer.MAX_VALUE;
+        while(begin <= end){
+            int mid = begin + (end - begin) / 2;
+            if(isPossible(weights, days, mid)){
+                end = mid - 1;
+            }
+            else{
+                begin = mid + 1;
+            }
+        }
+        return begin;
+    }
+}
