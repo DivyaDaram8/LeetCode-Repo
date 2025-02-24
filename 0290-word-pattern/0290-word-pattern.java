89d70a343eddf2c745a01e83608d9c2b6a56453d
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-02-24 23:02:38 +0530

    Time: 1 ms (83.13%), Space: 41.8 MB (18.97%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.java
+++ 0290-word-pattern/0290-word-pattern.java
@@ -6,18 +6,26 @@ class Solution {
         char[] chars = pattern.toCharArray();
         if(words.length != chars.length) return false;
         for(int i = 0; i < chars.length; i++){
-            if(hm.containsKey(chars[i])){
-                if(!hm.get(chars[i]).equals(words[i])){
-                    return false;
-                }
+            // if(hm.containsKey(chars[i])){
+            //     if(!hm.get(chars[i]).equals(words[i])){
+            //         return false;
+            //     }
+            // }
+            // else{
+            //     if(revMap.containsKey(words[i])){
+            //         return false;
+            //     }
+            //     hm.put(chars[i], words[i]);
+            //     revMap.put(words[i], chars[i]);
+            // }  
+            if(hm.containsKey(chars[i]) && !hm.get(chars[i]).equals(words[i])){
+                return false;
             }
-            else{
-                if(revMap.containsKey(words[i])){
-                    return false;
-                }
-                hm.put(chars[i], words[i]);
-                revMap.put(words[i], chars[i]);
-            }  
+            if(revMap.containsKey(words[i]) && revMap.get(words[i]) != chars[i]){
+                return false;
+            }
+            hm.put(chars[i], words[i]);
+            revMap.put(words[i], chars[i]);
         }
         return true;
     }
