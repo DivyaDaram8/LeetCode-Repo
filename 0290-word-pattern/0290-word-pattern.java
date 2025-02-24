c669646ff7d27c410400d70e7a395f34cea1cd04
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-02-24 22:58:32 +0530

    Time: 0 ms (100%), Space: 41.8 MB (18.97%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap <Character, String> hm = new HashMap<>();
+        HashMap <String, Character> revMap = new HashMap<>();
+        String[] words = s.split(" ");
+        char[] chars = pattern.toCharArray();
+        if(words.length != chars.length) return false;
+        for(int i = 0; i < chars.length; i++){
+            if(hm.containsKey(chars[i])){
+                if(!hm.get(chars[i]).equals(words[i])){
+                    return false;
+                }
+            }
+            else{
+                if(revMap.containsKey(words[i])){
+                    return false;
+                }
+                hm.put(chars[i], words[i]);
+                revMap.put(words[i], chars[i]);
+            }  
+        }
+        return true;
+    }
+}
