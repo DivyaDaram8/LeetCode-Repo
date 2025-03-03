e79c63c12f914094042c4aed2a5bade8841ccbd7
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-03-03 22:53:15 +0530

    Time: 4 ms (58.35%), Space: 45.1 MB (30.52%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,28 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-    
-        s = s.toLowerCase();
-        if(s.isEmpty())
-            return true;
-        int start = 0;
-        int end = s.length() - 1;
-        while(start <= end){
-            char S = s.charAt(start);
-            char E = s.charAt(end);
-            if(S >= 97&&S <= 122 || S >= 48&& S<= 57){
-                if(E >= 97&& E <= 122 || E >=48 && E <= 57){
-                    if( S != E){
-                        return false;
-                    }
-                    start++;
-                    end--;
-                }
-                else{
-                end--;
-                }
+         s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')){
+                sb.append(ch);
+            }
+        }
+        sb.toString().toCharArray();
+        int left = 0;
+        int right = sb.length() - 1;
+        while(left < right){
+            if(sb.charAt(left) != sb.charAt(right)){
+                return false;
             }
             else{
-            start++;
+                left++;
+                right--;
             }
         }
         return true;
