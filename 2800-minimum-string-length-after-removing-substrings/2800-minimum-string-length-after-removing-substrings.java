c5ef87111507eefe348fd332bdf64eb47e7270ac
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-04-04 23:38:48 +0530

    Time: 4 ms (87.78%), Space: 43.7 MB (29.36%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -1,23 +1,27 @@
 class Solution {
     public int minLength(String s) {
         Stack<Character> st = new Stack<>();
+        // for(char ch : s.toCharArray()){
+        //     st.push(ch);
+        //     if(st.size() >= 2){
+        //         char topEle = st.pop();
+        //         if((st.peek() == 'A' && topEle == 'B') ||
+        //              (st.peek() == 'C' && topEle == 'D')){
+        //                 st.pop();
+        //         }else{
+        //             st.push(topEle);
+        //         }
+        //     }
+        // }
+
+
         for(char ch : s.toCharArray()){
-            st.push(ch);
-            if(st.size() >= 2){
-                char topEle = st.pop();
-                if((st.peek() == 'A' && topEle == 'B') ||
-                     (st.peek() == 'C' && topEle == 'D')){
-                        st.pop();
-                }else{
-                    st.push(topEle);
-                }
+            if(!st.isEmpty() && ((ch == 'B' && st.peek() == 'A') || (ch == 'D' && st.peek() == 'C'))){
+                st.pop();
+            }else{
+                st.push(ch);
             }
         }
-        // StringBuilder sb = new StringBuilder();
-        // while(!st.isEmpty()){
-        //     sb.insert(0,st.pop());
-        // }
-        // return sb.toString().length();
         return st.size();
     }
 }
