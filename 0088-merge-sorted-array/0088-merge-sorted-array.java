08be06debd6bca58730cb384a03abc946d7ab9ff
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-04-27 10:15:34 +0530

    Time: 0 ms (100%), Space: 42.3 MB (60.7%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,30 +1,28 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int pntr1 = 0;
-        int pntr2 = 0;
-        int temp =  0;
-        while(pntr1 < m && pntr2 < n){
+        int pntr1 = m - 1;
+        int pntr2 = n - 1;
+        int lastIndex = m + n - 1;
+
+        while (pntr1 >= 0 && pntr2 >= 0) {
             int ele1 = nums1[pntr1];
             int ele2 = nums2[pntr2];
-            if(ele1 > ele2){
-                temp = nums1[pntr1];
-                nums1[pntr1] = nums2[pntr2];
-                nums2[pntr2] = temp;
-                
-                int k = pntr2;
-                while(k + 1 < n && nums2[k] > nums2[k+1]){
-                    int temp2 = nums2[k];
-                    nums2[k] = nums2[k+1];
-                    nums2[k+1] = temp2;
-                    k++;
-                }
+
+            if (ele1 > ele2) {
+                nums1[lastIndex] = ele1;
+                pntr1--;
+            } else {
+                nums1[lastIndex] = ele2;
+                pntr2--;
             }
-            pntr1++;  
+            lastIndex--;
         }
-        while(pntr2 < n){
-            nums1[pntr1] += nums2[pntr2];
-            pntr2++;
-            pntr1++;  
-        }  
+
+        // If nums2 still has elements left
+        while (pntr2 >= 0) {
+            nums1[lastIndex] = nums2[pntr2];
+            pntr2--;
+            lastIndex--;
+        }
     }
 }
