d8b99dd0fe273b6f7fde4941e07505b2820cd0e8
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-04-29 14:36:28 +0530

    Time: 0 ms (100%), Space: 42.1 MB (96.78%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,15 +10,26 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode curr = head;
-        ListNode prev = null;
-        ListNode next;
-        while(curr != null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        // ListNode curr = head;
+        // ListNode prev = null;
+        // ListNode next;
+        // while(curr != null){
+        //     next = curr.next;
+        //     curr.next = prev;
+        //     prev = curr;
+        //     curr = next;
+        // }
+        // return prev;
+
+        if(head == null || head.next == null){
+            return head;
         }
-        return prev;
+
+        ListNode newHead = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
     }
 }
