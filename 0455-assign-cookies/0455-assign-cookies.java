bbe0628e484f533b65b9be7b71a0f1f41c26591f
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-05-02 09:33:57 +0530

    Time: 9 ms (99.48%), Space: 48.2 MB (89.73%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-       Arrays.sort(g);
-       Arrays.sort(s);
-       int i = 0;
-       for(int j = 0; i < g.length && j < s.length; j++){
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        for(int j = 0; j < s.length && i < g.length; j++){
             if(g[i] <= s[j]){
                 i++;
-            }   
+            }
         }
         return i;
     }
