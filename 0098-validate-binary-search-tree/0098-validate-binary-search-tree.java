48cf11ca510e53471f295a7976996de76c972156
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-05-11 11:03:22 +0530

    Time: 0 ms (100%), Space: 43.2 MB (88.57%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,16 +14,17 @@
  * }
  */
 class Solution {
-    public boolean isTrue(TreeNode root, long min, long max){
+    public boolean checkBST(TreeNode root, long min, long max){
         if(root == null) return true;
         if(root.val <= min || root.val >= max){
             return false;
         }
-        boolean left = isTrue(root.left, min, root.val);
-        boolean right = isTrue(root.right, root.val, max);
+        boolean left = checkBST(root.left, min, root.val);
+        boolean right = checkBST(root.right, root.val, max);
         return left && right;
     }
+
     public boolean isValidBST(TreeNode root) {
-        return isTrue(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return checkBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 }
