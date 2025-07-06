71cbffd027a4768eb246499bbec0a64381eb0955
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-06 11:16:33 +0530

    Time: 5 ms (53.46%), Space: 45.1 MB (23.58%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,21 +1,97 @@
-class Solution {
-    public int romanToInt(String s) {
-        Map<Character, Integer> mp = new HashMap<>();
-        mp.put('I',1);
-        mp.put('V',5);
-        mp.put('X',10);
-        mp.put('L',50);
-        mp.put('C',100);
-        mp.put('D',500);
-        mp.put('M',1000);
-        
-        int sum = 0;
+class Solution{
+    public int romanToInt(String s){
+        Map<Character, Integer> romans = new HashMap<>();
+        romans.put('I', 1);
+        romans.put('V', 5);
+        romans.put('X', 10);
+        romans.put('L', 50);
+        romans.put('C', 100);
+        romans.put('D', 500);
+        romans.put('M', 1000);
+        int ans = 0;
         for(int i = 0; i < s.length(); i++){
-            if( i < s.length() - 1 && mp.get(s.charAt(i)) < mp.get(s.charAt(i+1)))
-                sum = sum - mp.get(s.charAt(i));
-            else
-                sum = sum + mp.get(s.charAt(i));
+            if(i + 1 < s.length() && romans.get(s.charAt(i)) < romans.get(s.charAt(i+1))){
+                ans -= romans.get(s.charAt(i));
+            }else{
+                ans += romans.get(s.charAt(i));
+            }
         }
-        return sum;
+        return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int romanToInt(String s) {
+//         Map<Character, Integer> romans = new HashMap<>();
+//         romans.put('I', 1);
+//         romans.put('V', 5);
+//         romans.put('X', 10);
+//         romans.put('L', 50);
+//         romans.put('C', 100);
+//         romans.put('D', 500);
+//         romans.put('M', 1000);
+        
+//         int idx = s.length() - 1;
+//         int ans = 0;
+        
+//         while (idx >= 0) {
+//             char ch = s.charAt(idx);
+//             if (idx - 1 >= 0) {
+//                 char ch1 = s.charAt(idx - 1);
+//                 if (ch == 'V' && ch1 == 'I') {
+//                     ans += 4;
+//                     idx -= 2;
+//                 } else if (ch == 'X' && ch1 == 'I') {
+//                     ans += 9;
+//                     idx -= 2;
+//                 } else if (ch == 'L' && ch1 == 'X') {
+//                     ans += 40;
+//                     idx -= 2;
+//                 } else if (ch == 'C' && ch1 == 'X') {
+//                     ans += 90;
+//                     idx -= 2;
+//                 } else if (ch == 'D' && ch1 == 'C') {
+//                     ans += 400;
+//                     idx -= 2;
+//                 } else if (ch == 'M' && ch1 == 'C') {
+//                     ans += 900;
+//                     idx -= 2;
+//                 } else {
+//                     ans += romans.get(ch);
+//                     idx--;
+//                 }
+//             } else {
+//                 ans += romans.get(ch);
+//                 idx--;
+//             }
+//         }
+//         return ans;
+//     }
+// }
