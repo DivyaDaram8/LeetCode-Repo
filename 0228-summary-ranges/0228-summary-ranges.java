f2546e439ee109a67d0cefb6d64a4b00ba17f2b0
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-06 16:03:13 +0530

    Time: 5 ms (82.14%), Space: 42.1 MB (12.65%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,27 +1,53 @@
-class Solution {
-    public List<String> summaryRanges(int[] nums) {
-        List<String> sol = new ArrayList<>();
+// class Solution {
+//     public List<String> summaryRanges(int[] nums) {
+//         List<String> sol = new ArrayList<>();
+//         int begin = 0;
+//         int end = 1;
+//         int counter = 0;
+//         while(end < nums.length){
+//             while(end < nums.length && nums[counter] + 1 == nums[end] ){
+//                 end++;
+//                 counter++;
+//             }
+//             if(end - begin == 1){
+//                 sol.add(String.valueOf(nums[begin]));
+//             }
+//             else{
+//                 sol.add(nums[begin]+"->"+nums[counter]);
+//             }
+//             begin = end;
+//             counter = end;
+//             end++;
+//         }
+//         if(begin == nums.length -1){
+//             sol.add(String.valueOf(nums[begin]));
+//         }
+//         return sol;
+//     }
+// }
+class Solution{
+    public List<String> summaryRanges(int[] nums){
+        List<String> ans = new ArrayList<>();
         int begin = 0;
         int end = 1;
         int counter = 0;
         while(end < nums.length){
-            while(end < nums.length && nums[counter] + 1 == nums[end] ){
+            while(end < nums.length && nums[counter] + 1 == nums[end]){
                 end++;
                 counter++;
             }
             if(end - begin == 1){
-                sol.add(String.valueOf(nums[begin]));
-            }
-            else{
-                sol.add(nums[begin]+"->"+nums[counter]);
+                ans.add(String.valueOf(nums[begin]));
+            }else{
+                ans.add(nums[begin]+"->"+nums[counter]);
             }
             begin = end;
             counter = end;
             end++;
         }
-        if(begin == nums.length -1){
-            sol.add(String.valueOf(nums[begin]));
+        if(begin == nums.length - 1){
+            ans.add(String.valueOf(nums[begin]));
         }
-        return sol;
+        return ans;
     }
 }
