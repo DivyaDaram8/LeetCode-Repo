1b7c42e8e5d785451c02c2b73938add64ef55cd0
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-06 11:50:20 +0530

    Time: 1 ms (93.27%), Space: 41.5 MB (54.84%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,25 +1,47 @@
-class Solution {
-    public boolean isSubsequence(String s, String t) {
-        int cntS = 0;
-        int cntT = 0;
+class Solution{
+    public boolean isSubsequence(String s, String t){
+        int idxS = 0;
+        int idxT = 0;
         int cnt = 0;
-        int lenT = t.length();
-        if(s.length() == 0){
-            return true;
-        }
-        while(--lenT >= 0){
-            char chS = s.charAt(cntS);
-            char chT = t.charAt(cntT);
-            if(chS == chT){
+        if(s.length() == 0) return true;
+        int len = t.length() - 1;
+        while(idxT <= len && idxS < s.length()){
+            if(s.charAt(idxS) == t.charAt(idxT)){
+                idxS++;
+                idxT++;
                 cnt++;
-                cntS++;
-                cntT++;
+            }else{
+                idxT++;
             }
-            else{
-                cntT++;
-            }
-            if(cnt == s.length()) return true;
         }
-        return false;
+        return cnt == s.length();
     }
 }
+
+
+
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+//         int cntS = 0;
+//         int cntT = 0;
+//         int cnt = 0;
+//         int lenT = t.length();
+//         if(s.length() == 0){
+//             return true;
+//         }
+//         while(--lenT >= 0){
+//             char chS = s.charAt(cntS);
+//             char chT = t.charAt(cntT);
+//             if(chS == chT){
+//                 cnt++;
+//                 cntS++;
+//                 cntT++;
+//             }
+//             else{
+//                 cntT++;
+//             }
+//             if(cnt == s.length()) return true;
+//         }
+//         return false;
+//     }
+// }
