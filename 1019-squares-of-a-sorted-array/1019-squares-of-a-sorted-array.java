383953d8c1c03de3ca14a45a9c8c4b521a0f4a56
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-08 18:11:04 +0530

    Time: 1 ms (100%), Space: 47.6 MB (23.09%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -1,27 +1,48 @@
-class Solution {
-    public int[] sortedSquares(int[] nums) {
-        // for(int i = 0; i < nums.length; i++){
-        //     if(nums[i] < 0){
-        //         nums[i] = nums[i] * nums[i];
-        //     }
-        //     else{
-        //         nums[i] = nums[i] * nums[i];
-        //     }
-        // }
-        // Arrays.sort(nums);
-        // return nums;
-        int left = 0;
-        int right = nums.length - 1;
-        int[] res = new int[nums.length];
-        for(int i = nums.length - 1; i >= 0; i--){
-            if(Math.abs(nums[right]) > Math.abs(nums[left])){
-                res[i] = nums[right] * nums[right];
-                right--;
+// class Solution {
+//     public int[] sortedSquares(int[] nums) {
+//         // for(int i = 0; i < nums.length; i++){
+//         //     if(nums[i] < 0){
+//         //         nums[i] = nums[i] * nums[i];
+//         //     }
+//         //     else{
+//         //         nums[i] = nums[i] * nums[i];
+//         //     }
+//         // }
+//         // Arrays.sort(nums);
+//         // return nums;
+//         int left = 0;
+//         int right = nums.length - 1;
+//         int[] res = new int[nums.length];
+//         for (int i = nums.length - 1; i >= 0; i--) {
+//             if (Math.abs(nums[right]) > Math.abs(nums[left])) {
+//                 res[i] = nums[right] * nums[right];
+//                 right--;
+//             } else {
+//                 res[i] = nums[left] * nums[left];
+//                 left++;
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+class Solution{
+    public int[] sortedSquares(int[] nums){
+        int start = 0;
+        int end = nums.length - 1;
+        int idx = end;
+        int[] res = new int[end + 1];
+        while(start <= end){ 
+            int endVal = nums[end] * nums[end];
+            int startVal = nums[start] * nums[start];
+            if( endVal >= startVal){
+                res[idx] = endVal;
+                end--;
+            }else{
+                res[idx] = startVal;
+                start++;
             }
-            else{
-                res[i] = nums[left] * nums[left];
-                left++;
-            }
+            idx--;
         }
         return res;
     }
