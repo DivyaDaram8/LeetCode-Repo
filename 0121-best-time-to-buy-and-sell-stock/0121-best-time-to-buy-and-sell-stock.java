b5ece3325f0e65cdd2c7dd7bbbb278d5a12da8aa
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-08 18:17:33 +0530

    Time: 2 ms (73.81%), Space: 61.7 MB (64.25%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,5 +1,17 @@
-class Solution {
-    public int maxProfit(int[] prices) {
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int minVal = Integer.MAX_VALUE;
+//         int maxProfit = 0;
+//         for(int i = 0; i < prices.length; i++){
+//             minVal = Math.min(minVal, prices[i]);
+//             maxProfit = Math.max(maxProfit, prices[i] - minVal);
+//         }
+//         return maxProfit;
+//     }
+// }
+
+class Solution{
+    public int maxProfit(int[] prices){
         int minVal = Integer.MAX_VALUE;
         int maxProfit = 0;
         for(int i = 0; i < prices.length; i++){
