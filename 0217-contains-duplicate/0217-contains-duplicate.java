e4728f6f52aa6636aea9871551d7be42d19c0e4d
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-08 18:21:50 +0530

    Time: 13 ms (88.27%), Space: 58.2 MB (62.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -10,14 +10,27 @@
 //         return false;
 //     }
 // }
-class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Set<Integer> seen = new HashSet<>();
-        for (int num : nums) {
-            if (seen.contains(num)) {
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Set<Integer> seen = new HashSet<>();
+//         for (int num : nums) {
+//             if (seen.contains(num)) {
+//                 return true;
+//             }
+//             seen.add(num);
+//         }
+//         return false;
+//     }
+// }
+
+class Solution{
+    public boolean containsDuplicate(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if(set.contains(nums[i])){
                 return true;
             }
-            seen.add(num);
+            set.add(nums[i]);
         }
         return false;
     }
