841b38024fe610c2eff73260080846a43404d00b
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-09 19:23:32 +0530

    Time: 1 ms (100%), Space: 44.6 MB (58.41%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,18 +1,60 @@
-class Solution {
-    public boolean isValidSudoku(char[][] board) {
+// class Solution {
+//     public boolean isValidSudoku(char[][] board) {
+//         for(int i = 0; i < 9; i++){
+//             int[] rowHash = new int[9];
+//             int[] colHash = new int[9];
+//             for(int j = 0; j < 9; j++){
+//                 if(board[i][j] != '.'){
+//                     int ele = board[i][j] - '1';
+//                     if(rowHash[ele] > 0){
+//                         return false;
+//                     }
+//                     rowHash[ele]++;
+//                 }
+//                 if(board[j][i] != '.'){
+//                     int ele = board[j][i] - '1';
+//                     if(colHash[ele] > 0){
+//                         return false;
+//                     }
+//                     colHash[ele]++;
+//                 }
+//             }
+//         }
+//         for(int block  = 0; block < 9; block++){
+//             int[] boxHash = new int[9];
+//             int rowStart = 3 * (block / 3);
+//             int colStart = 3 * (block % 3);
+//             for(int i = rowStart; i < rowStart + 3; i++){
+//                 for(int j = colStart; j < colStart + 3; j++){
+//                     if(board[i][j] != '.'){
+//                         int ele = board[i][j] - '1';
+//                         if(boxHash[ele] > 0){
+//                             return false;
+//                         }
+//                         boxHash[ele]++;
+//                     }
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+class Solution{
+    public boolean isValidSudoku(char[][] board){
         for(int i = 0; i < 9; i++){
-            int[] rowHash = new int[9];
-            int[] colHash = new int[9];
+            int[] rowHash = new int[10];
+            int[] colHash = new int[10];
             for(int j = 0; j < 9; j++){
                 if(board[i][j] != '.'){
-                    int ele = board[i][j] - '1';
+                    int ele = board[i][j] - '0';
                     if(rowHash[ele] > 0){
                         return false;
                     }
                     rowHash[ele]++;
                 }
                 if(board[j][i] != '.'){
-                    int ele = board[j][i] - '1';
+                    int ele = board[j][i] - '0';
                     if(colHash[ele] > 0){
                         return false;
                     }
@@ -20,18 +62,18 @@ class Solution {
                 }
             }
         }
-        for(int block  = 0; block < 9; block++){
-            int[] boxHash = new int[9];
+        for(int block = 0; block < 9; block++){
+            int[] boxHash = new int[10];
             int rowStart = 3 * (block / 3);
             int colStart = 3 * (block % 3);
             for(int i = rowStart; i < rowStart + 3; i++){
                 for(int j = colStart; j < colStart + 3; j++){
                     if(board[i][j] != '.'){
-                        int ele = board[i][j] - '1';
-                        if(boxHash[ele] > 0){
+                        int num = board[i][j] - '0';
+                        if(boxHash[num] > 0){
                             return false;
                         }
-                        boxHash[ele]++;
+                        boxHash[num]++;
                     }
                 }
             }
