edb8bc760821ba68c8179e2ce1df43ea00607043
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-10 18:46:59 +0530

    Time: 4 ms (56.59%), Space: 43.3 MB (61.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,48 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-         s = s.toLowerCase();
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//          s = s.toLowerCase();
+//         StringBuilder sb = new StringBuilder();
+//         for(char ch : s.toCharArray()){
+//             if((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')){
+//                 sb.append(ch);
+//             }
+//         }
+//         sb.toString().toCharArray();
+//         int left = 0;
+//         int right = sb.length() - 1;
+//         while(left < right){
+//             if(sb.charAt(left) != sb.charAt(right)){
+//                 return false;
+//             }
+//             else{
+//                 left++;
+//                 right--;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
+class Solution{
+    public boolean isPalindrome(String s){
+        s = s.toLowerCase();
         StringBuilder sb = new StringBuilder();
-        for(char ch : s.toCharArray()){
+        char ch;
+        for(int i = 0; i < s.length(); i++){
+            ch = s.charAt(i);
             if((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')){
                 sb.append(ch);
             }
         }
         sb.toString().toCharArray();
-        int left = 0;
-        int right = sb.length() - 1;
-        while(left < right){
-            if(sb.charAt(left) != sb.charAt(right)){
+        int start = 0;
+        int end = sb.length() - 1;
+        while(start < end){
+            if(sb.charAt(start) != sb.charAt(end)){
                 return false;
-            }
-            else{
-                left++;
-                right--;
+            }else{
+                start++;
+                end--;
             }
         }
         return true;
