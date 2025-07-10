1094f0a63df64f638896039a73352267c474aae3
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-10 19:47:18 +0530

    Time: 0 ms (100%), Space: 41.9 MB (32%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -5,22 +5,36 @@
 //     }
 // }
 
+// class Solution{
+//     public int lengthOfLastWord(String s){
+//         s = s.trim();
+//         int start = 0;
+//         int end = 0;
+//         int len = 0;
+//         while(end < s.length()){
+//             while( end < s.length() && s.charAt(end) != ' '){
+//                 end++;
+//             }
+//             len = end - start;
+//             start = end;
+//             while(start < s.length() && s.charAt(start) == ' '){
+//                 start++;
+//             }
+//             end = start;
+//         }
+//         return len;
+//     }
+// }
+
+
 class Solution{
     public int lengthOfLastWord(String s){
         s = s.trim();
-        int start = 0;
-        int end = 0;
         int len = 0;
-        while(end < s.length()){
-            while( end < s.length() && s.charAt(end) != ' '){
-                end++;
-            }
-            len = end - start;
-            start = end;
-            while(start < s.length() && s.charAt(start) == ' '){
-                start++;
-            }
-            end = start;
+        int i = s.length() - 1;
+        while(i >= 0 && s.charAt(i) != ' '){
+            i--;
+            len++;
         }
         return len;
     }
