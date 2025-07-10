0dd2c2a2d2bd194495dd9bd2812ecb354a286e9b
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-10 19:47:54 +0530

    Time: 2 ms (12.35%), Space: 42 MB (32%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -5,37 +5,37 @@
 //     }
 // }
 
-// class Solution{
-//     public int lengthOfLastWord(String s){
-//         s = s.trim();
-//         int start = 0;
-//         int end = 0;
-//         int len = 0;
-//         while(end < s.length()){
-//             while( end < s.length() && s.charAt(end) != ' '){
-//                 end++;
-//             }
-//             len = end - start;
-//             start = end;
-//             while(start < s.length() && s.charAt(start) == ' '){
-//                 start++;
-//             }
-//             end = start;
-//         }
-//         return len;
-//     }
-// }
-
-
 class Solution{
     public int lengthOfLastWord(String s){
         s = s.trim();
+        int start = 0;
+        int end = 0;
         int len = 0;
-        int i = s.length() - 1;
-        while(i >= 0 && s.charAt(i) != ' '){
-            i--;
-            len++;
+        while(end < s.length()){
+            while( end < s.length() && s.charAt(end) != ' '){
+                end++;
+            }
+            len = end - start;
+            start = end;
+            while(start < s.length() && s.charAt(start) == ' '){
+                start++;
+            }
+            end = start;
         }
         return len;
     }
 }
+
+
+// class Solution{
+//     public int lengthOfLastWord(String s){
+//         s = s.trim();
+//         int len = 0;
+//         int i = s.length() - 1;
+//         while(i >= 0 && s.charAt(i) != ' '){
+//             i--;
+//             len++;
+//         }
+//         return len;
+//     }
+// }
