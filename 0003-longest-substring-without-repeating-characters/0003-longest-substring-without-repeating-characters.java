cd11ead529b267933f7158eb6b13468a97b8a985
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-11 21:19:37 +0530

    Time: 7 ms (35.17%), Space: 44.8 MB (43.54%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,21 +1,44 @@
-class Solution {
-    public int lengthOfLongestSubstring(String s) {
-        HashSet <Character> hs = new HashSet<>(); 
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         HashSet <Character> hs = new HashSet<>(); 
+//         int ans = 0;
+//         int start = 0;
+//         for(int end = 0; end < s.length(); end++){
+//             char ch = s.charAt(end);
+//             if(!hs.contains(ch)){
+//                 hs.add(ch);
+//             }
+//             else{
+//                 while(hs.contains(ch)){
+//                     hs.remove(s.charAt(start));
+//                     start++;
+//                 }
+//                 hs.add(s.charAt(end));
+//             }
+//             ans = Math.max(ans, end - start + 1);
+//         }
+//         return ans;
+//     }
+// }
+
+class Solution{
+    public int lengthOfLongestSubstring(String s){
+        HashSet<Character> hs = new HashSet<>();
         int ans = 0;
         int start = 0;
-        for(int end = 0; end < s.length(); end++){
-            char ch = s.charAt(end);
-            if(!hs.contains(ch)){
-                hs.add(ch);
-            }
-            else{
-                while(hs.contains(ch)){
+        int end = 0;
+        while(end < s.length()){
+            if(!hs.contains(s.charAt(end))){
+                hs.add(s.charAt(end));
+            }else{
+                while(hs.contains(s.charAt(end))){
                     hs.remove(s.charAt(start));
                     start++;
                 }
                 hs.add(s.charAt(end));
             }
             ans = Math.max(ans, end - start + 1);
+            end++;
         }
         return ans;
     }
