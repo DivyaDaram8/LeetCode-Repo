909ffaa7fb2f0ffa5db49d429e9dc8e3caa596d5
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-14 19:33:08 +0530

    Time: 13 ms (74.65%), Space: 45.3 MB (84.71%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -1,16 +1,32 @@
-class Solution {
-    public String removeDuplicates(String s) {
-        Stack<Character> st = new Stack<>();
-        for(int i = 0; i < s.length(); i++){
-            if(!st.isEmpty() && st.peek() == s.charAt(i)){
-                st.pop();
-            }else{
-                st.push(s.charAt(i));
-            }
-        }
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         Stack<Character> st = new Stack<>();
+//         for(int i = 0; i < s.length(); i++){
+//             if(!st.isEmpty() && st.peek() == s.charAt(i)){
+//                 st.pop();
+//             }else{
+//                 st.push(s.charAt(i));
+//             }
+//         }
+//         StringBuilder sb = new StringBuilder();
+//         while(!st.isEmpty()){
+//             sb.insert(0, st.pop());
+//         }
+//         return sb.toString();
+//     }
+// }
+
+class Solution{
+    public String removeDuplicates(String s){
         StringBuilder sb = new StringBuilder();
-        while(!st.isEmpty()){
-            sb.insert(0, st.pop());
+        int len;
+        for(int i = 0; i < s.length(); i++){
+            len = sb.length();
+            if(len > 0 && sb.charAt(len - 1) == s.charAt(i)){
+                sb.deleteCharAt(len - 1);
+            }else{
+                sb.append(s.charAt(i));
+            }
         }
         return sb.toString();
     }
