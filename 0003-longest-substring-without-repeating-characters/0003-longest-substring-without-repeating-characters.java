db1829bf58312a2309153a2e291075a6efdae328
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-22 21:46:22 +0530

    Time: 9 ms (17.05%), Space: 43.4 MB (94.34%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,45 +1,18 @@
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         HashSet <Character> hs = new HashSet<>(); 
-//         int ans = 0;
-//         int start = 0;
-//         for(int end = 0; end < s.length(); end++){
-//             char ch = s.charAt(end);
-//             if(!hs.contains(ch)){
-//                 hs.add(ch);
-//             }
-//             else{
-//                 while(hs.contains(ch)){
-//                     hs.remove(s.charAt(start));
-//                     start++;
-//                 }
-//                 hs.add(s.charAt(end));
-//             }
-//             ans = Math.max(ans, end - start + 1);
-//         }
-//         return ans;
-//     }
-// }
-
-class Solution{
-    public int lengthOfLongestSubstring(String s){
-        HashSet<Character> hs = new HashSet<>();
-        int ans = 0;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
         int start = 0;
-        int end = 0;
-        while(end < s.length()){
-            if(!hs.contains(s.charAt(end))){
-                hs.add(s.charAt(end));
-            }else{
-                while(hs.contains(s.charAt(end))){
-                    hs.remove(s.charAt(start));
-                    start++;
-                }
-                hs.add(s.charAt(end));
+        int maxLen = 0;
+        for(int end = 0; end < s.length(); end++){
+            char ch = s.charAt(end);
+            while(hm.containsKey(ch) && hm.get(ch) > 0){
+                char leftChar = s.charAt(start);
+                hm.put(leftChar, hm.get(leftChar) - 1);
+                start++;
             }
-            ans = Math.max(ans, end - start + 1);
-            end++;
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+            maxLen = Math.max(maxLen, end - start + 1);
         }
-        return ans;
+        return maxLen;
     }
 }
