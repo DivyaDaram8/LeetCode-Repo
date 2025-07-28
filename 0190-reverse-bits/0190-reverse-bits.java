6106a90fda7b307dddc505003de5db6aa6f1653e
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-28 19:48:12 +0530

    Time: 0 ms (100%), Space: 41.1 MB (87.74%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,21 +1,33 @@
+// class Solution {
+//     public int reverseBits(int n) {
+//         StringBuilder sb = new StringBuilder();
+//         while(n != 0){
+//             if((n & 1) == 1){
+//                 sb.append('1');
+//             }else{
+//                 sb.append('0');
+//             }
+//             n = n >> 1;
+//         }
+
+//         while(sb.length() < 32){
+//             sb.append('0');
+//         }
+
+//         String str = sb.toString();
+//         long num = Long.parseLong(str, 2);
+//         return (int)num;
+//     }
+// }
+
 class Solution {
-    public int reverseBits(int n) {
-        StringBuilder sb = new StringBuilder();
-        while(n != 0){
-            if((n & 1) == 1){
-                sb.append('1');
-            }else{
-                sb.append('0');
-            }
-            n = n >> 1;
+    int result = 0;
+    public int reverseBits(int n){
+        for(int i = 0; i < 32; i++){
+            result = result << 1;
+            result = result | (n & 1);
+            n = n >>> 1;
         }
-
-        while(sb.length() < 32){
-            sb.append('0');
-        }
-
-        String str = sb.toString();
-        long num = Long.parseLong(str, 2);
-        return (int)num;
+        return result;
     }
 }
