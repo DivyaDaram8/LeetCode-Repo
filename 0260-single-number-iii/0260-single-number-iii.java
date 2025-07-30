59050685f589d7c7a3d431b3e1676ded85be7fda
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-07-30 19:01:32 +0530

    Time: 1 ms (99.7%), Space: 46 MB (17.18%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+        for(int i = 0; i < nums.length; i++){
+            xor ^= nums[i];
+        }
+
+        int diffBit = xor & -xor;
+        int a = 0;
+        int b = 0;
+        for(int i = 0; i < nums.length; i++){
+            if((nums[i] & diffBit) != 0){
+                a ^= nums[i];
+            }else{
+                b ^= nums[i];
+            }
+        }
+        return new int[]{a, b};
+    }
+}
