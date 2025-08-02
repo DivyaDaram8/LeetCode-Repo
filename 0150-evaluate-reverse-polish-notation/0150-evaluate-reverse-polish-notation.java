b7ab2d1ec06ca27c3e68d1f73fdc3c5ea1a9d7f5
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-08-02 22:31:38 +0530

    Time: 6 ms (72.48%), Space: 45 MB (22.72%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,25 +1,22 @@
 class Solution {
-    int resolves(int a, int b, char operator) {
+    public int calcs(int a, int b, char operator){
         if (operator == '+') return a + b;
-        else if (operator == '-') return a - b;
+        else if(operator == '-') return a - b;
         else if (operator == '*') return a * b;
-        return a / b; // Division truncates toward zero by default in Java
+        return a / b;
     }
-
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
-        
-        for (String token : tokens) {
-            if (token.length() == 1 && token.charAt(0) < '0') {
-                int b = stack.pop();
-                int a = stack.pop();
-                int result = resolves(a, b, token.charAt(0));
+        for(String token : tokens){
+            if(token.length() == 1 && token.charAt(0) < '0'){
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                int result = calcs(num1, num2, token.charAt(0));
                 stack.push(result);
-            } else {
+            }else{
                 stack.push(Integer.parseInt(token));
             }
         }
-        
         return stack.pop();
     }
 }
