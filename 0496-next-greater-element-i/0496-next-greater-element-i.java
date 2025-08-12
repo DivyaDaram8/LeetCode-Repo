74bf029d62280c5091fc3f31d6d410ad489b5bf5
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-08-12 21:48:54 +0530

    Time: 3 ms (92.3%), Space: 44.2 MB (40.68%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,25 +1,32 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> st = new Stack<>();
-        HashMap<Integer,Integer> mp = new HashMap<>();
-        for(int i = nums2.length-1 ; i>=0 ; i--){
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        for(int i = nums2.length - 1; i >= 0; i--){
             if(st.isEmpty()){
                 st.push(nums2[i]);
-                mp.put(nums2[i],-1);
-            }
-            else{
-                if(st.peek()>nums2[i]){
-                    mp.put(nums2[i],st.peek());
+                mp.put(nums2[i], -1);
+            }else{
+                if(st.peek() > nums2[i]){
+                    mp.put(nums2[i], st.peek());
                     st.push(nums2[i]);
                     continue;
+                }else{
+                    while(!st.isEmpty() && st.peek() < nums2[i]){
+                        st.pop();
+                    }
+                    if(st.isEmpty()){
+                        mp.put(nums2[i], -1);
+                    }else{
+                        mp.put(nums2[i], st.peek());
+                    }
                 }
-                else while(!st.isEmpty() && st.peek()<nums2[i]) st.pop();
-                if(st.isEmpty()) mp.put(nums2[i],-1);
-                else mp.put(nums2[i],st.peek());
                 st.push(nums2[i]);
             }
         }
-        for(int i = 0 ; i<nums1.length ; i++) nums1[i] = mp.get(nums1[i]);
+        for(int i = 0; i < nums1.length; i++){
+            nums1[i] = mp.get(nums1[i]);
+        }
         return nums1;
     }
 }
