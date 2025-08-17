329df01de82b83cbd7d93b75af51c8c0e52e9a86
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-08-17 16:23:08 +0530

    Time: 1 ms (71.08%), Space: 42.2 MB (63.33%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,19 +15,24 @@
  */
 class Solution {
     public List<Integer> bfs(TreeNode root, List<Integer> res){
-        if(root == null) return res;
+        if(root == null){
+            return res;
+        }
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
         while(!queue.isEmpty()){
-            int levelSize = queue.size(); 
+            int levelSize = queue.size();
             for(int i = 0; i < levelSize; i++){
                 TreeNode curr = queue.poll();
                 if(i == levelSize - 1){
                     res.add(curr.val);
                 }
-
-                if(curr.left != null) queue.add(curr.left);
-                if(curr.right != null) queue.add(curr.right);
+                if(curr.left != null){
+                    queue.add(curr.left);
+                }
+                if(curr.right != null){
+                    queue.add(curr.right);
+                }
             }
         }
         return res;
