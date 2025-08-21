a06e283d7cdfc72ea008cc03f59cacefe6557c1f
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-08-21 22:20:32 +0530

    Time: 6 ms (55.01%), Space: 42.6 MB (29.48%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.java
@@ -8,13 +8,13 @@ class Solution {
                 hm.put(ch, hm.getOrDefault(ch, 0) + 1);
             }
         }
+
         if(!hm.containsKey('b')|| !hm.containsKey('a') || !hm.containsKey('l')
         || !hm.containsKey('o') || !hm.containsKey('n')){
             return res;
         }else{
-           res =  Math.min(Math.min(hm.get('b'), hm.get('a')),  
-                    (Math.min(hm.get('l') / 2, 
-                    Math.min(hm.get('o') / 2, hm.get('n')))));
+            res = Math.min(hm.get('b'), 
+                            (Math.min(hm.get('a'), (Math.min(hm.get('l') / 2, (Math.min(hm.get('o') / 2, hm.get('n'))))))));
         }
         return res;
     }
