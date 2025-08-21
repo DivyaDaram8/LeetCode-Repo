3b503c2dd80b0408027d49b7357ff05a90cf1828
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-08-21 22:39:14 +0530

    Time: 1 ms (99.9%), Space: 44.7 MB (39.95%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,47 +1,5 @@
-// class Solution {
-//     public boolean isValidSudoku(char[][] board) {
-//         for(int i = 0; i < 9; i++){
-//             int[] rowHash = new int[9];
-//             int[] colHash = new int[9];
-//             for(int j = 0; j < 9; j++){
-//                 if(board[i][j] != '.'){
-//                     int ele = board[i][j] - '1';
-//                     if(rowHash[ele] > 0){
-//                         return false;
-//                     }
-//                     rowHash[ele]++;
-//                 }
-//                 if(board[j][i] != '.'){
-//                     int ele = board[j][i] - '1';
-//                     if(colHash[ele] > 0){
-//                         return false;
-//                     }
-//                     colHash[ele]++;
-//                 }
-//             }
-//         }
-//         for(int block  = 0; block < 9; block++){
-//             int[] boxHash = new int[9];
-//             int rowStart = 3 * (block / 3);
-//             int colStart = 3 * (block % 3);
-//             for(int i = rowStart; i < rowStart + 3; i++){
-//                 for(int j = colStart; j < colStart + 3; j++){
-//                     if(board[i][j] != '.'){
-//                         int ele = board[i][j] - '1';
-//                         if(boxHash[ele] > 0){
-//                             return false;
-//                         }
-//                         boxHash[ele]++;
-//                     }
-//                 }
-//             }
-//         }
-//         return true;
-//     }
-// }
-
-class Solution{
-    public boolean isValidSudoku(char[][] board){
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
         for(int i = 0; i < 9; i++){
             int[] rowHash = new int[10];
             int[] colHash = new int[10];
@@ -66,6 +24,7 @@ class Solution{
             int[] boxHash = new int[10];
             int rowStart = 3 * (block / 3);
             int colStart = 3 * (block % 3);
+
             for(int i = rowStart; i < rowStart + 3; i++){
                 for(int j = colStart; j < colStart + 3; j++){
                     if(board[i][j] != '.'){
