617b504ec5157c781901af92bc8cd4b1ce107b47
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-08-22 22:16:27 +0530

    Time: 0 ms (100%), Space: 46.2 MB (14.26%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -1,22 +1,18 @@
 class Solution {
-    public int idx(int[] nums, int target){
-        int begin = 0;
-        int end = nums.length-1;
-        while(begin <= end){
-            int mid = end + (begin - end) /2;
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) /2;
             if(nums[mid] == target){
                 return mid;
             }
-            else if(target > nums[mid]){
-                begin = mid+1;
-            }
-            else{
-                end = mid -1;
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
             }
         }
         return -1;
-    }
-    public int search(int[] nums, int target) {
-        return idx(nums, target);
     }
 }
